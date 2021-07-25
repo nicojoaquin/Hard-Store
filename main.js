c1032700e84d89f92cfdb310e7a9146c8f618e46
nicojoaquin <nicojoaquin1998@gmail.com>
2021-07-25 12:54:05 -0300

    clase1

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-let nombre = "Nicolás";
-var alumno = "Nico";
-var cantAlumnos = 80;
-var noInicializada;
-const CURSO = "js";
-alert (nombre);
+// let nombre = "Nicolás";
+// var alumno = "Nico";
+// var cantAlumnos = 80;
+// var noInicializada;
+// const CURSO = "js";
+// alert (nombre);
 
 
-var num1 = 2;
-var num2 = 3;
+// var num1 = 2;
+// var num2 = 3;
 
-var resultado = num1 + num2;
-alert (resultado)
+// var resultado = num1 + num2;
+// alert (resultado)
 
-var resta = num1 - num2;
+// var resta = num1 - num2;
 
-alert (resta);
+// alert (resta);
 
-var multiplicacion = num1 * num2;
+// var multiplicacion = num1 * num2;
 
-alert (multiplicacion);
+// alert (multiplicacion);
 
-var cadena1 = " Coder";
-var cadena2 = " House";
+// var cadena1 = " Coder";
+// var cadena2 = " House";
 
-var resultadoCadenas = cadena1 + cadena2;
+// var resultadoCadenas = cadena1 + cadena2;
 
-alert (resultadoCadenas);
+// alert (resultadoCadenas);
 
-var resultado2 = "Hola" + cadena1;
+// var resultado2 = "Hola" + cadena1;
  
-alert (resultado2);
+// alert (resultado2);
 
-console.log (resultado2)
+// console.log (resultado2);
+
+var respuestaUsuario = prompt ("¿Cual es tu nombre?");
+alert ("Hola " + respuestaUsuario);
+console.log ("Hola " + respuestaUsuario);
