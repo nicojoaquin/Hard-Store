a827048779ee283e33aaa72b7e011dfe116bd863
nicojoaquin <nicojoaquin1998@gmail.com>
2021-07-25 12:41:11 -0300

    iniciando proyecto

---
+++ main.js
@@ -0,0 +1,34 @@
+let nombre = "Nicolás";
+var alumno = "Nico";
+var cantAlumnos = 80;
+var noInicializada;
+const CURSO = "js";
+alert (nombre);
+
+
+var num1 = 2;
+var num2 = 3;
+
+var resultado = num1 + num2;
+alert (resultado)
+
+var resta = num1 - num2;
+
+alert (resta);
+
+var multiplicacion = num1 * num2;
+
+alert (multiplicacion);
+
+var cadena1 = " Coder";
+var cadena2 = " House";
+
+var resultadoCadenas = cadena1 + cadena2;
+
+alert (resultadoCadenas);
+
+var resultado2 = "Hola" + cadena1;
+ 
+alert (resultado2);
+
+console.log (resultado2)
