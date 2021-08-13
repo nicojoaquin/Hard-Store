0d214699a24accb725a4f4606eaf0dbc6c3be4a4
nicojoaquin <nicojoaquin1998@gmail.com>
2021-08-12 23:13:46 -0300

    buy

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
- //Función de validación
- 
- let actual = 2021;
- const edadMayor = 18;
- let nombre = prompt ("¿Cual es tu nombre?");
- let edad = parseInt (prompt ("Año de nacimiento"));
- let respuesta3 = parseInt((actual - edad));
+//Función de validación
+ let year = 2021;
+ const OLD = 18;
+ let name1 = prompt ("¿Cual es tu nombre?");
+ let age = parseInt (prompt ("Año de nacimiento"));
+ let answer = parseInt((year - age));
 
- function edadAcceso() {
- if (respuesta3 > edadMayor) {
- return "Bienvenido " + nombre + "!";
- }
+ function validate() {
+ if (answer > OLD & age > 1920) {
+   return buy();
+ } 
  else {
  return "Tienes que ser mayor de edad para acceder";
 }
  }
-alert(edadAcceso());
+
+alert(validate());
+
+//Función de compra
+ function buy() {
+  alert("Bienvenido " + name1 + "!");
+  let articles = ["PC", "Notebooks", "Monitores", "Headset"];
+  let quote = "Elije un articulo para comprar:"
+  let choose = parseInt(prompt(quote + "\n" + "1: " + articles[0] + ": $100000" + "\n" + "2: " + articles[1] + ": $90000" + "\n" + "3: " + articles[2] + ": $40000" + "\n" + "4: " + articles[3] + ": $10000"));
+
+   switch(choose) {
+    case 1:
+      return "Gracias por comprar el articulo: " + articles[0];
+
+    case 2:
+      return "Gracias por comprar el articulo: " + articles[1];
+      
+    case 3:
+      return "Gracias por comprar el articulo: " + articles[2];
+      
+    case 4:
+      return "Gracias por comprar el articulo: " + articles[3];
+      
+    default:
+      alert("Elije una opcion correcta");
+   }
+  }
+ 
 
 
-//Función de calculadora
-if(respuesta3 > edadMayor) {
-let a = parseInt(prompt("Primer numero"))
-let operacion =prompt('Ingrese signo de operación: "+", "-", "*", "/"'); 
-let b = parseInt(prompt("Segundo numero"))
-
-const calc = (a, b,) => {
-switch(operacion) {
-case operacion = "+":
-    return a + b;
-
-case operacion = "-":
-    return a - b;
-
-case operacion = "*":
-    return a * b;
-
-case operacion = "/":
-    return a / b;
-
-default: 
-   alert("Ingrese una operacion valida");
-}
-}
-console.log(calc(a, b));
-}
