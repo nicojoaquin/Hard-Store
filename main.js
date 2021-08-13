ed3f66347d49632d53bc0ef8dac9261e9b879c97
nicojoaquin <nicojoaquin1998@gmail.com>
2021-08-13 20:02:59 -0300

    fullBuy

--- main.js
+++ main.js
@@ -14,27 +14,53 @@
 }
  }
 
+ 
 alert(validate());
+
 
 //Función de compra
  function buy() {
   alert("Bienvenido " + name1 + "!");
-  let articles = ["1: PC", "2: Notebooks", "3: Monitores", "4: Headset"];
+  let articles = ["1: PC", "2: Notebook", "3: Monitor", "4: Headset"];
   let quote = "Elije un articulo para comprar:"
-  let choose = parseInt(prompt(quote + "\n" + (articles.join( "\n" ))));
+  let choose = parseInt(prompt(quote + "\n" + articles.join("\n")));
 
    switch(choose) {
     case 1:
-      return "Gracias por comprar el articulo: " + articles[0];
-
+      conf = prompt("Costo: $100000" + "\n" + "Desea comprar?");
+      if(conf == "si") {
+        return "Gracias por comprar el articulo" + articles[0].slice(1);
+       }
+       else {
+         return "Siga navegando";
+       }
+       
     case 2:
-      return "Gracias por comprar el articulo: " + articles[1];
+      conf = prompt("Costo: $90000" + "\n" + "Desea comprar?");
+      if(conf == "si") {
+        return "Gracias por comprar el articulo" + articles[1].slice(1);
+   }
+      else {
+        return "Siga navegando";
+  }
       
     case 3:
-      return "Gracias por comprar el articulo: " + articles[2];
+      conf = prompt("Costo: $40000" + "\n" + "Desea comprar?");
+      if(conf == "si") {
+       return "Gracias por comprar el articulo" + articles[2].slice(1);
+}
+      else {
+       return "Siga navegando";
+}
       
     case 4:
-      return "Gracias por comprar el articulo: " + articles[3];
+      conf = prompt("Costo: $11000" + "\n" + "Desea comprar?");
+      if(conf == "si") {
+       return "Gracias por comprar el articulo" + articles[3].slice(1);
+}
+      else {
+       return "Siga navegando";
+}
       
     default:
       alert("Elije una opcion correcta");
