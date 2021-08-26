a034d1db0bad59cef362c6c1a0ffbc5b499063c4
nicojoaquin <nicojoaquin1998@gmail.com>
2021-08-26 02:15:01 -0300

    filtering

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-//Responsive menu 
+//Variables
+const allDocument = document.getElementsByTagName("*");
 let active = document.getElementById('activeClass');
-let item = document.getElementsByClassName('store__category--item');
+let item = $('.store__category--item');
+let products = $('.store__products--item');
+let showAll = $('.store__category--item[category="all"]');
 const navMenu = document.getElementById('nav-menu');
 const navClick = document.getElementById('nav__hamburger');
+let footer = document.getElementById('foot');
 
+//Responsive menu 
 navClick.addEventListener('click', () => {
-  navMenu.classList.toggle('show_menu')
+  navMenu.classList.toggle('show_menu');
+  allDocument[0].classList.toggle('menu_off'); 
 })
 
-//Color change menu
-function links(index, element, remove){
-  for (let i = index; i < item.length; i++){
-    item[i].addEventListener('click', () =>{
-      element.classList.remove(remove);
+//Change menu
+function links(element, remove){
+  item.click(function(){
+    footer.classList.add('footerRelative')
+    allDocument[0].classList.remove('menu_off'); 
+    element.classList.remove(remove);
+    let catProduct = $(this).attr('category');
+    console.log(catProduct);
+
+    //Ocultar todo
+    function hideProduct(){
+      products.hide();
+      products.css('transform', 'scale(0)');
+    } setTimeout(hideProduct, 100);
+
+    //Mostrar cada producto
+    function showProduct(){
+      $(`.store__products--item[category=${catProduct}]`).show();
+      $(`.store__products--item[category=${catProduct}]`).css('transform', 'scale(1)');
+    } setTimeout(showProduct, 100);
+  })
+    //Mostrar todos los productos
+    showAll.click(function(){
+    function setAll(){
+      products.show();
+      products.css('transform', 'scale(1)');
+    } setTimeout(setAll, 100);
     })
-    }
+
 }
-links(0, navMenu, 'show_menu')
-links(1, active, 'active');
+links(navMenu, 'show_menu')
+links(active, 'active');
