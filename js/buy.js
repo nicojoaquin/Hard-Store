fda468ec98076229652c6403f58b4320ee73c60d
nicojoaquin <nicojoaquin1998@gmail.com>
2021-08-29 10:55:18 -0300

    searchDesign

--- js/buy.js
+++ js/buy.js
@@ -1,33 +1,34 @@
 //Buscador
 let form = document.getElementById('finder');
-let boton = document.getElementById('boton');
 let input = document.getElementById('input');
 
 form.addEventListener('submit', (e) => {  //Busca la palabra escrita en el input
-  e.preventDefault()
-  let value = form.input.value
+  e.preventDefault();
+  let formulario = e.target;
+  let value = formulario.input.value;
   console.log(value); //Imprime en la consola la palabra escrita 
 });
 
 //Simulacion de compra
 class Article{
-    constructor(nombre, precio, stock) { 
-    this.nombre = nombre;
-    this.precio = precio;
+    constructor(id, price, stock) { 
+    this.id = id;
+    this.price = price;
     this.stock = stock;
   }
 }
 
 //Elementos a crear
-const desktop = [new Article('PC MSI', `$${155000}`, 5,), new Article('PC Asus', `$${133200}`, 7)]
-const notebook = [new Article("Notebook HP", `$${174600}`, 13), new Article("Notebook AlienWare", `$${410000}`, 9) ]
-const monitor = [new Article("Monitor Samsung", `$${22300}`, 18), new Article("Monitor Viewsonic", `$${44000}`, 15)]
-const headset = [new Article("Headset Razer" , `$${17500}`, 24), new Article("Headset Hyperx" , `$${11000}`, 31)]
+const desktop = [new Article('PC MSI', `$${155000}`, 3,), new Article('PC Asus', `$${133200}`, 7)];
+const notebook = [new Article("Notebook HP", `$${174600}`, 6), new Article("Notebook AlienWare", `$${410000}`, 2) ];
+const monitor = [new Article("Monitor Samsung", `$${22300}`, 4), new Article("Monitor Viewsonic", `$${44000}`, 5)];
+const headset = [new Article("Headset Razer" , `$${17500}`, 9), new Article("Headset Hyperx" , `$${11000}`, 8)];
 
 let elements = [desktop, notebook, monitor, headset];  //Listamos los elementos para utilizarlos
 
 //Funcion que imprime en la consola cada articulo que presionamos
 function evento(article, a, b) {
+  console.log(elements[a][b]);
   document.getElementById(article).addEventListener('click', () => {
     while (elements[a][b].stock > 0) {
       let ask = parseInt(prompt(`Precio: ${elements[a][b].precio}\nDesea comprar?\n1: Si`))  //Confirmacion de compra 
