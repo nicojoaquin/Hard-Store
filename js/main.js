f92233aac57efaee44b3f731dc4619b95efae4a4
nicojoaquin <nicojoaquin1998@gmail.com>
2021-09-07 11:59:03 -0300

    creadorDinamico

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
-//Variables
+//Variables.
 const main = document.body;
+const store = document.querySelector('.store__products');
 const form = document.querySelector('#finder'),
       input = document.querySelector('#input');
-
-const productos = document.querySelector('.store__products');
 
 const carScreen = document.querySelector('#carScreen'),
       counter = document.querySelector('#counter'),
       car = document.querySelector('#car');    
-let cartNumber = 1
 
 const mainBars = document.querySelector('#mainBars'),
       newBars = document.querySelector('#newBar'),
       menu = document.querySelector('#mainMenu');  
 
+
 //Menu principal
+
 mainBars.addEventListener('click', () => {
+
+//Al hacer click en las barras, se agrega una nueva clase y aparece el menu.
   menu.classList.add('menuShow');
   main.classList.add('newBody');
   mainBars.classList.remove('bars');
   main.style.overflow="hidden"
 })
+
 newBars.addEventListener('click', () => {
+  
+//Al hacer click en las cruz, se elimina la clase agregada y desaparece el menu.
   menu.classList.remove('menuShow')
   main.classList.remove('newBody');
   mainBars.classList.add('bars');
   main.style.overflow="auto"
 })
 
+  
 //Buscador
 form.addEventListener('submit', (e) => {                     //Busca la palabra escrita en el input.
   e.preventDefault();
@@ -35,6 +41,7 @@ form.addEventListener('submit', (e) => {                     //Busca la palabra 
   input.value = ''                                           //Vacia el campo.
 })
 
+  
 //Carrito
 car.addEventListener('click', () => {
   carScreen.classList.add('carShow')
@@ -43,29 +50,60 @@ document.getElementById('times').addEventListener('click', () => {
   carScreen.classList.remove('carShow');
 })
 
-//Simulacion de compra.
-class Article{
-    constructor(id, name, price, stock) { 
+  
+//Creador de productos. 
+class Article {
+  constructor (id, name, price, cat, stock) {
     this.id = id;
     this.name = name;
     this.price = price;
-    this.stock = stock;
+    this.cat = cat
+    this.stock = stock
   }
 }
+const msi = new Article ('msi', 'Pc de escritorio: MSI', `$155000`, 'desktop', 3);
+const asus = new Article ('asus', 'Pc de escritorio: Asus Rog', `$270000`, 'desktop', 2);
+const aorus = new Article ('aorus', 'Pc de escritorio: Gigabyte Aorus', '$240000', 'desktop', 2);
+const hp = new Article ('hp', 'Notebook HP Omen', `$182500`, 'notebooks', 4);
+const alienWare = new Article ('alienware', 'Notebook AlienWare', `$330850`, 'notebooks', 1);
+const samsung = new Article ('samsung', 'Monitor Samsung', `$22000`, 'monitors', 3);
+const viewSonic = new Article ('viewsonic', 'Monitor ViewSonic 144HZ', `$39000`, 'monitors', 3);
+const hyperX = new Article ('hyperx', 'Headset HyperX', `$11300`, 'headsets', 5);
+const razer = new Article ('razer', 'Headset Razer', `$15000`, 'headsets', 6);
 
-//Elementos a crear
-const msi = new Article('msi', 'PC de escritorio MSI', `$${155000}`, 3,);
-const asus =  new Article('asus', 'PC de escritorio Asus ROG',  `$${133200}`, 2);
-const hp = new Article("hp", 'Notebook HP Omen', `$${174600}`, 4) 
-const alienWare = new Article("alienWare", 'Notebook Alienware', `$${410000}`, 2) ;
-const samsung = new Article("samsung", 'Monitor Samsung', `$${22300}`, 4) 
-const viewSonic = new Article("viewSonic", 'Monitor Viewsonic 144hz', `$${44000}`, 5);
-const razer = new Article("razer" , 'Headset Razer', `$${17500}`, 5) 
-const hyperx = new Article("hyperx" , 'Headset Hyperx', `$${11000}`, 4);
+let articles = [msi, asus, aorus, hp, alienWare, samsung, viewSonic, hyperX, razer,]; //Array de los productos creados.
 
-let elements = [msi, asus, hp, alienWare, samsung, viewSonic, razer, hyperx];  //Listamos los elementos para utilizarlos.
+articles.forEach(art => {
+ 
+//Creamos los elementos que iran adentro de la tienda.  
+const createEl = document.createElement('div');
+const createImg = document.createElement('img');
+const createP = document.createElement('p');
+const createH3 = document.createElement('h3');
 
-//Alerta que se ejecuta cuando agregamos un producto.
+createEl.classList.add('store__products--item');
+createEl.setAttribute('category', art.cat);
+                                                 
+createImg.classList.add ('imgClass');
+createImg.src = `./assets/images/${art.id}.jpg`;
+                                                 //Les asignamos las clases y atributos para que coincidan con las propiedades de la clase "Article".
+createP.innerHTML = ` ${art.name} <br /> <span>${art.price}</span>`
+
+createH3.classList.add('addCart');
+createH3.setAttribute('id', art.id);  
+createH3.textContent = 'Agregar al carrito';
+
+//Agregamos los elementos creados con sus clases y propiedades, al HTML.
+store.append(createEl);  
+createEl.append(createImg); 
+createEl.append(createP);
+createEl.append(createH3);
+});
+
+
+//Alertas personalizadas.
+
+//Se ejecuta cuando agregamos un producto.
 function addAlert(articulo) {
   Swal.fire({
     title: "Agregado!",
@@ -78,8 +116,8 @@ function addAlert(articulo) {
     confirmButtonColor: 'blue'
   })
 }
-
-//Alerta que se ejecuta cuando no hay mas stock.
+  
+//Se ejecuta cuando no hay mas stock.
 function cantAlert() {
   swal.fire({
     title: "No hay mas stock!",
@@ -93,52 +131,54 @@ function cantAlert() {
   })
 }   
 
-////Funciónes que restan el stock del producto y agregan carrito en storage
+  
+//Funciónes que restan el stock del producto y agregan carrito en storage
+  
 const stockSub = (el) => {
-    el.stock -= 1
-    console.log(el)   
-  }
-
+  el.stock -= 1      //Resta el stock.
+  console.log(el)   
+}
+  
 const cartNumbers = () => {
-  let productNumbers = parseInt(localStorage.getItem("cart"));  //Almacenamos el valor del storage.
-
+  let productNumbers = parseInt(sessionStorage.getItem("cart"));  //Almacenamos el valor del storage.
+  
   if(productNumbers){
-    localStorage.setItem('cart', productNumbers + 1); //Si ya existe el valor, al hacer click, se suma uno.
+    sessionStorage.setItem('cart', productNumbers + 1); //Si ya existe el valor, al hacer click, se suma uno.
     counter.textContent = productNumbers + 1
   } else{
-    localStorage.setItem('cart', 1);  //Obtenemos del storage la cantidad 1 en el carrito al crear el storage.
+    sessionStorage.setItem('cart', 1);  //Obtenemos del storage la cantidad 1 en el carrito al crear el storage.
     counter.textContent = 1;
   }
 }
-
+  
 const loadCart = () => {  //Al recargar la pagina, sigue el valor del carrito con el mismo valor del storage.
-  let productNumbers = parseInt(localStorage.getItem("cart"));
-
+  let productNumbers = parseInt(sessionStorage.getItem("cart"));
+  
   if(productNumbers) {
     counter.textContent = productNumbers;
   }
 }
 loadCart();
 
+  
 //Agrega al carrito al hacer click.
-productos.addEventListener('click', (e) =>{
-  e.preventDefault();
+store.addEventListener('click', (e) =>{
+  articles.forEach(el => {
 
-  elements.forEach(elem => {
-    if(e.target.tagName === 'H3' && e.target.id == elem.id){      
-      if (elem.stock > 0 ) {
-        addAlert(elem.name)
-        stockSub(elem)
-        cartNumbers() //Sumamos el carrito cuando clickeamos en un producto.
+    if(e.target.tagName === 'H3' && e.target.id == el.id){   
+
+      if (el.stock > 0 ) {
+        addAlert(el.name);
+        stockSub(el);
+        cartNumbers(); //Sumamos el carrito cuando clickeamos en un producto.
+        
       } else {
         cantAlert();
-        throw 'No hay mas stock!'
+        throw 'No hay mas stock!';
       }
+
     }
-  });  
+
+  })
 })
 
-
-  
-
-  
